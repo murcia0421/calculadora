58c726f5c89f4d164a32c42289cf440a88fc577b
murcia0421 <112002572+murcia0421@users.noreply.github.com>
2024-03-16 09:05:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
 
     try {
       this.setState({
-        result: (eval(checkResult) || "") + ""
+        result: ""
       })
     } catch(e) {
       this.setState({
